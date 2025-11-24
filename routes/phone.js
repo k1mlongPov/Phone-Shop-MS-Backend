364d7880bb4869ac3286b77acbb1b13f84575d3a
ពៅ គីមឡុង <kimlong3385@gmail.com>
2025-11-24 13:42:41 +0700

    Update upload image to cloudinary

--- routes/phone.js
+++ routes/phone.js
@@ -4,16 +4,26 @@ const phoneCtrl = require('../controllers/phoneController');
 const authMiddleware = require('../middleware/authMiddleware');
 const upload = require('../middleware/uploadMiddleware');
 
-// Create phone (supports multiple images)
-router.post('/', authMiddleware, upload.array('images', 5), phoneCtrl.createPhone);
-
-// Update phone (replace/add images)
-router.put('/:id', authMiddleware, upload.array('images', 5), phoneCtrl.updatePhone);
-
-// other routes...
 router.get('/', authMiddleware, phoneCtrl.listPhones);
 router.get('/:id', authMiddleware, phoneCtrl.getPhoneById);
+
 router.delete('/:id', authMiddleware, phoneCtrl.deletePhone);
+
 router.post('/:id/restock', authMiddleware, phoneCtrl.adjustStock);
+
+router.post(
+    '/',
+    authMiddleware,
+    upload.array('images', 5),
+    phoneCtrl.createPhone
+);
+
+router.put(
+    '/:id',
+    authMiddleware,
+    upload.array('images', 5),
+    phoneCtrl.updatePhone
+);
+
 
 module.exports = router;
